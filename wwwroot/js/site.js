0c695226cc159f480c95fb4ee08f0a660592220d
itariss <itariss@protonmail.com>
2022-09-25 22:26:07 -0300

    Iniciando Front-end

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(() => {
+	let statusList = document.querySelectorAll(".status");
+
+	statusList.forEach(status => {
+		let descricaoContainer = status.parentElement;
+
+		if (status.innerHTML.includes("Finalizado")) {
+			descricaoContainer.classList.add("line-through");
+			descricaoContainer.classList.add("text-danger");
+
+			("line-through");
+		} else {
+			descricaoContainer.classList.remove("line-through");
+			descricaoContainer.classList.remove("line-through");
+		}
+	});
+})();
